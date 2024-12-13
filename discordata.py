f064f4f236e79a093c006380d8648e529092465a
metanull-operator <54516310+metanull-operator@users.noreply.github.com>
2024-12-12 22:17:36 -0700

    Formatting fixes

--- discordata.py
+++ discordata.py
@@ -389,7 +389,7 @@ def format_message(applicant_id, event_type, screening_status, wallet_address, i
     message = "### New Applicant Status\n"
     message += f"**Program:** {program_title}\n"
 
-    message += "**Review Status:** "
+    message += "**Review Status:**    "
     if screening_status == "GREEN":
         message += ":white_check_mark:\n"
     elif screening_status == "RED":
@@ -406,7 +406,7 @@ def format_message(applicant_id, event_type, screening_status, wallet_address, i
     else:
         message += "N/A\n"
 
-    message += "**Risk Score:** "
+    message += "\n**Risk Score:**       "
     if screening_status == "GREEN" and address_score:
         if address_score >= acceptable_risk_score:
             message += ":green_circle:\n"
@@ -415,12 +415,12 @@ def format_message(applicant_id, event_type, screening_status, wallet_address, i
     else:
         message += "N/A\n"
 
-    message += "**Event:***\n"
+    message += "\n**Event:**\n"
     message += f"Applicant ID: {applicant_id}\n"
     message += f"Timestamp: {current_time} UTC\n"
     message += f"Event Type: {event_type}\n"
 
-    message += "**Wallet:**\n"
+    message += "\n**Wallet:**\n"
     message += f"Wallet Address: {wallet_address}\n"
     message += "Risk Score: "
     if screening_status == "GREEN" and address_score:
